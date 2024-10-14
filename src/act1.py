debc7767e6c8a2438413745b912a77d9ad593013
99abeel <scdvabel@iesrafaelalberti.es>
2024-10-14 21:35:46 +0200

    sdaf

---
+++ src/act1.py
@@ -0,0 +1,50 @@
+
+def comprobar_numero(entrada: str) ->  bool:
+    if entrada.startswith("-"):
+        entrada = entrada[1:]
+
+    return entrada.isdigit()
+
+
+def introducir_numero(msj: str) -> (bool, int):
+    entrada = input(msj).strip().lower()
+    
+    if comprobar_numero(entrada):
+        num = int(entrada)
+    else:
+        if entrada.lower() == "fin":
+            return True, None
+        else: 
+            return False, None
+    
+
+
+def main():
+    cont = 0
+    suma = 0
+    media = 0
+
+    encuentra_fin = False
+    while not encuentra_fin:
+        entrada
+    salir = False
+
+    while not salir:
+        entrada_correcta, numero = introducir_numero("Introduzca un numero: ")
+        if entrada_correcta and numero is not None: 
+            cont += 1
+            suma += numero
+        elif entrada_correcta and numero is None:
+            encuentra_fin = True
+        else:
+            print("Entrada invalida")
+        if cont > 0:
+            media = suma/cont
+        print("{}{}{}".format(suma, cont, media))
+
+
+
+
+
+if __name__=="__main__":
+    main()
